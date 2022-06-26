57f418f27598e69001217e0bb6006fa96c14abfa
David Towster <davidtowster@Davids-MacBook-Pro.local>
2022-06-26 14:47:35 -0700

    Add ejs library, replace .html views with .ejs views

--- app.js
+++ app.js
@@ -3,27 +3,25 @@ const express = require('express');
 // Express app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
+// Configure port to listen to
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // .sendFile() expects an absolute path by default, so set the root filepath
-    // to __dirname (i.e. the current directory)
-    res.sendFile('./views/index.html', { root: __dirname });
-    // .sendFile() sends response to browser such that the app will 
-    // not match to any other routes if it is invoked
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// redirect
-app.get('/about-me', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render();
 });
 
 // 404 Page
 app.use((req, res) => {
-    // If nothing above has matched, will match to this .use() method
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
