f4c1dcfd67b7604bb6f624a22efa27683ee10984
David Towster <davidtowster@Davids-MacBook-Pro.local>
2022-06-26 17:24:11 -0700

    Use ejs to add titles to all pages

--- app.js
+++ app.js
@@ -10,18 +10,24 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'This is a snippet'},
+        {title: 'Mario finds stars', snippet: 'This is a snippet'},
+        {title: 'How to defeat bowser', snippet: 'This is a snippet'},
+    ];
+
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create' });
 });
 
 // 404 Page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
